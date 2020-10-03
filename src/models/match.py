4c447061a871d5e47b9cd84a1b9e70c8aede9bec
Amit Botzer <amitbotzer92@gmail.com>
2020-10-03 22:08:37 +0300

    basic match constructor

--- src/models/match.py
+++ src/models/match.py
@@ -1,4 +1,10 @@
+import datetime
+
 class Match():
 
-    def __init__(self):
-        pass
+    def __init__(self, home_team: str, away_team: str, home_goals: int, away_goals: int, start_time: datetime.datetime):
+        self.home_team = home_team
+        self.away_team = away_team
+        self.home_goals = home_goals
+        self.away_goals = away_goals
+        self.start_time = start_time
